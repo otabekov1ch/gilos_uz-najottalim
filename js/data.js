2e11911ecc307717cd29a4d517a43c0360d7fab4
otabekov1ch <88477479+otabekov1ch@users.noreply.github.com>
2022-06-21 17:56:15 +0500

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,109 @@
+const products = [
+  {
+    id: 123,
+    title: "Redmi Note 10 Pro",
+    img: "https://picsum.photos/300/200",
+    price: 4300000,
+    model: "Xiaomi",
+    addedDate: new Date("2021-11-12").toISOString(),
+    benefits: ["8gb", "128gb", "Waterproof"]
+  },
+  {
+    id: 124,
+    title: "Samsung Note 20 Ultra",
+    img: "https://picsum.photos/300/200",
+    price: 8300000,
+    model: "Samsung",
+    addedDate: new Date("2021-10-12").toISOString(),
+    benefits: ["32gb", "1tb"]
+  },
+  {
+    id: 120,
+    title: "Redmi Note 10 Pro",
+    img: "https://picsum.photos/300/200",
+    price: 4300000,
+    model: "Xiaomi, Redmi",
+    addedDate: new Date("2021-11-12").toISOString(),
+    benefits: ["8gb", "128gb", "Waterproof", "Super Cheap"]
+  },
+  {
+    id: 121,
+    title: "Samsung Note 20 Ultra",
+    img: "https://picsum.photos/300/200",
+    price: 8300000,
+    model: "Samsung",
+    addedDate: new Date("2021-10-12").toISOString(),
+    benefits: ["12gb", "1tb", "Super Cam"]
+  },
+  {
+    id: 125,
+    title: "Xiaomi Poco X3 Pro",
+    img: "https://picsum.photos/300/200",
+    price: 3589000,
+    model: "Xiaomi, Poco",
+    addedDate: new Date("2021-09-12").toISOString(),
+    benefits: ["8gb", "256gb", "For PUBG Mobile", "Snapdragon"]
+  },
+
+
+  {
+    id: 126,
+    title: "Xiaomi Poco F3",
+    img: "https://picsum.photos/300/200",
+    price: 5300000,
+    model: "Xiaomi, Poco",
+    addedDate: new Date("2022-01-12").toISOString(),
+    benefits: ["8gb", "256gb", "Premium Design", "Snapdragon"]
+  },
+  {
+    id: 127,
+    title: "Apple iPhone 13 Pro Max",
+    img: "https://picsum.photos/300/200",
+    price: 15300000,
+    model: "Apple",
+    addedDate: new Date("2022-03-26").toISOString(),
+    benefits: ["8gb", "1tb", "Super Cam", "Respect", "Bionic"]
+  },
+  {
+    id: 128,
+    title: "Xiaomi Black Shark 4 Pro",
+    img: "https://picsum.photos/300/200",
+    price: 11300000,
+    model: "Xiaomi",
+    addedDate: new Date("2022-05-06").toISOString(),
+    benefits: ["16gb", "512gb", "For Gaming", "Super energy"]
+  },
+  {
+    id: 129,
+    title: "Apple iPhone 12 Pro Max",
+    img: "https://picsum.photos/300/200",
+    price: 13405000,
+    model: "Apple",
+    addedDate: new Date("2021-10-12").toISOString(),
+    benefits: ["6gb", "256gb", "Respect", "Premium Design"]
+  },
+  {
+    id: 130,
+    title: "Samsung Galaxy S22 Ultra",
+    img: "https://picsum.photos/300/200",
+    price: 18300000,
+    model: "Samsung",
+    addedDate: new Date("2022-10-12").toISOString(),
+    benefits: ["16gb", "1tb", "Super Cam", "Ultra Design"]
+  }
+]
+
+const manufacturers = [
+  {
+    id: 1,
+    name: "Xiaomi"
+  },
+  {
+    id: 2,
+    name: "Apple"
+  },
+  {
+    id: 3,
+    name: "Samsung"
+  }
+];
